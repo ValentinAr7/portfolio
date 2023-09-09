f52c692ec709ab5cfd2f4eae23e47955aad34e2c
ValentinAr7 <104101252+ValentinAr7@users.noreply.github.com>
2023-09-09 12:44:05 +0300

    [Portfolio] Image Section

--- portfolio/src/components/profile-section/profile.js
+++ portfolio/src/components/profile-section/profile.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./profile.module.css";
-import profileImage from "../../../public/logo.png";
+import profileImage from "../../images/logo.png";
 
 const Profile = () => {
   return (
     <div className={styles.container}>
       <div className={styles.profileSection}>
-        <img src={profileImage} />
+        <img src={profileImage} alt="Logo" className={styles.profileImage} />
+        <h2>Valentin Arnautski</h2>
       </div>
     </div>
   );
