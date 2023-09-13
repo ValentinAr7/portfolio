5a56ab6968f0568b77f0038269faa21b42ee5146
ValentinAr7 <104101252+ValentinAr7@users.noreply.github.com>
2023-09-13 17:28:42 +0300

    [Portfolio] skills section

--- portfolio/src/components/main-section/Skills.js
+++ portfolio/src/components/main-section/Skills.js
@@ -10,37 +10,37 @@ import CampaignIcon from "@mui/icons-material/Campaign";
 const skillsData = [
   {
     title: "Web Development",
-    icon: <WebDevelopmentIcon />,
+    icon: <WebDevelopmentIcon fontSize="large" />,
     description:
       "Proficient in HTML, CSS, JavaScript, and modern web frameworks like React and Angular. Skilled in building responsive and user-friendly web applications.",
   },
   {
     title: "Web Design",
-    icon: <WebDesignIcon />,
+    icon: <WebDesignIcon fontSize="large" />,
     description:
       "Experienced in digital marketing strategies, including SEO, SEM, social media marketing, and email marketing. Strong analytical skills to measure and optimize campaign performance.",
   },
   {
     title: "Mobile Applications",
-    icon: <MobileApplicationsIcon />,
+    icon: <MobileApplicationsIcon fontSize="large" />,
     description:
       "Experienced in digital marketing strategies, including SEO, SEM, social media marketing, and email marketing. Strong analytical skills to measure and optimize campaign performance.",
   },
   {
     title: "Trading and Investing",
-    icon: <MonetizationOnIcon />,
+    icon: <MonetizationOnIcon fontSize="large" />,
     description:
       "Experienced in digital marketing strategies, including SEO, SEM, social media marketing, and email marketing. Strong analytical skills to measure and optimize campaign performance.",
   },
   {
     title: "Sales",
-    icon: <AttachMoneyIcon />,
+    icon: <AttachMoneyIcon fontSize="large" />,
     description:
       "Experienced in digital marketing strategies, including SEO, SEM, social media marketing, and email marketing. Strong analytical skills to measure and optimize campaign performance.",
   },
   {
     title: "Marketing",
-    icon: <CampaignIcon />,
+    icon: <CampaignIcon fontSize="large" />,
     description:
       "Experienced in digital marketing strategies, including SEO, SEM, social media marketing, and email marketing. Strong analytical skills to measure and optimize campaign performance.",
   },
@@ -49,9 +49,9 @@ const skillsData = [
 const Skills = () => {
   const skillElements = skillsData.map((data, index) => (
     <div key={index} className={styles.skillContainer}>
-      <p>{data.icon}</p>
+      <p className={styles.icon}>{data.icon}</p>
       <div>
-        <span className={styles.title}>{data.title}</span>
+        <span className={styles.heading}>{data.title}</span>
         <p className={styles.description}>{data.description}</p>
       </div>
     </div>
